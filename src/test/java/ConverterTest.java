010e2875180e1d0b9e7109eed7ed104b57e0ff72
NerixGO <nerix.main@gmail.com>
2026-07-18 16:16:23 +0200

    !!! Now Using MAVEN for Java (CI tests for jdk)  !!!

--- src/test/java/ConverterTest.java
+++ src/test/java/ConverterTest.java
@@ -1,11 +1,49 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 public class ConverterTest {
 
-    public static void main(String[] args) {
 
-        assert Converter.celsiusToFahrenheit(0) == 32.0f;
-        assert Converter.celsiusToFahrenheit(100) == 212.0f;
-        assert Converter.fahrenheitToCelsius(32) == 0.0f;
+    @Test
+    void convertsCelsiusToFahrenheit() {
 
-        System.out.println("All tests passed!");
+        assertEquals(32.0f, Converter.celsiusToFahrenheit(0));
+        assertEquals(212.0f, Converter.celsiusToFahrenheit(100));
+
+    }
+
+
+    @Test
+    void convertsFahrenheitToCelsius() {
+
+        assertEquals(0.0f, Converter.fahrenheitToCelsius(32));
+        assertEquals(100.0f, Converter.fahrenheitToCelsius(212));
+
+    }
+
+
+    @Test
+    void convertsKilometersToMiles() {
+
+        assertEquals(0.621371f, Converter.kmToMiles(1));
+
+    }
+
+
+    @Test
+    void convertsMilesToKilometers() {
+
+        assertEquals(1.60934f, Converter.milesToKm(1));
+
+    }
+
+
+    @Test
+    void convertsKilogramsToPounds() {
+
+        assertEquals(2.20462f, Converter.kgToPounds(1));
+
     }
 }
